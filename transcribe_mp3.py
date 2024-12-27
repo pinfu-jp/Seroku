5de214184bc8f9cb87b062bb233fb5c33b051741
h.a <hidetaka.asahi.3@gmail.com>
2024-12-27 22:20:19 +0900

    README 更新など

--- transcribe_mp3.py
+++ transcribe_mp3.py
@@ -159,10 +159,8 @@ if __name__ == '__main__':
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
     mp3_path = os.path.join(script_dir, 'tmp', 'input', 'test123.mp3')
-    # mp3_folder = os.path.join(script_dir, 'tmp', 'output')
     output_text_file = os.path.join(script_dir, 'tmp', 'output', 'output.txt')
 
-    # transcribe_audio_files(mp3_folder, output_text_file)
     tanscribe_audio_file(mp3_path, output_text_file)
 
 
